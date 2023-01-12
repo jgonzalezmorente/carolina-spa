c29489100b82b1beab33aa5c0c2af7ca1976f0f9
José Antonio González Morente <jgonzalezmorente@gmail.com>
2023-01-12 11:13:29 +0100

    CarolinaSPA

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper', {
+    effect: 'cube',
+    // grabCursor: true,
+    loop: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+  });
